6856bba19688f82aef70c604c86fbe9f154ddc56
Mwaura Patrick <74305949+MwauraPatrick@users.noreply.github.com>
2024-04-21 17:52:22 +0300

    new

--- dvpapp/src/routes/Content/+page.svelte
+++ dvpapp/src/routes/Content/+page.svelte
@@ -1,9 +1,9 @@
 <svelte:head>
-    <title>Content</title>
-    <meta name="content" content="main content" />
+	<title>Content</title>
+	<meta name="content" content="main content" />
 </svelte:head>
 
 <div class="maincontent">
-    <h1>Main Content</h1>
-    <p>This is the main content of your page.</p>
+	<h1>Main Content</h1>
+	<p>This is the main content of your page.</p>
 </div>
