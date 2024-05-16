6a003d5ea3bbd88475828ba77fedc73ee2b3f826
Mwaura Patrick <74305949+MwauraPatrick@users.noreply.github.com>
2024-05-16 21:18:20 +0300

    updates

--- dvpapp/.netlify/server/nodes/1.js
+++ dvpapp/.netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DtQPXx1X.js","_app/immutable/chunks/scheduler.DW9KkVYI.js","_app/immutable/chunks/index.zzyvqDcF.js","_app/immutable/chunks/stores.Ctdt7QYZ.js","_app/immutable/chunks/entry.80nk73ey.js","_app/immutable/chunks/index.C774K7Pp.js"];
+export const imports = ["_app/immutable/nodes/1.UdWqYwJL.js","_app/immutable/chunks/scheduler.DW9KkVYI.js","_app/immutable/chunks/index.zzyvqDcF.js","_app/immutable/chunks/stores.DvZHWfbN.js","_app/immutable/chunks/entry.CD-BGUz_.js","_app/immutable/chunks/index.C774K7Pp.js"];
 export const stylesheets = [];
 export const fonts = [];
