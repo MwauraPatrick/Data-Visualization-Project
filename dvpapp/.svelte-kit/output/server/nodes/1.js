f8ce913306806a3305c4e826502a2c6e2d19e68b
Mwaura Patrick <74305949+MwauraPatrick@users.noreply.github.com>
2024-05-17 12:55:18 +0300

    undid the gitignore
    undid the gitignore

--- dvpapp/.svelte-kit/output/server/nodes/1.js
+++ dvpapp/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CcB-Ika7.js","_app/immutable/chunks/scheduler.DW9KkVYI.js","_app/immutable/chunks/index.zzyvqDcF.js","_app/immutable/chunks/stores.9jpuPlXt.js","_app/immutable/chunks/entry.CZac0gQ7.js","_app/immutable/chunks/index.C774K7Pp.js"];
+export const imports = ["_app/immutable/nodes/1.SOXtpMYd.js","_app/immutable/chunks/scheduler.DW9KkVYI.js","_app/immutable/chunks/index.zzyvqDcF.js","_app/immutable/chunks/stores.BerxIuLR.js","_app/immutable/chunks/entry.9Z0pPfAU.js","_app/immutable/chunks/index.C774K7Pp.js"];
 export const stylesheets = [];
 export const fonts = [];
--- dvpapp/build/_app/immutable/entry/start.BggQoQ9I.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CZac0gQ7.js";export{t as start};
